3b6b471b6f5e89ade2b356d44498cb4e02601f81
Will <wjbeshore@gmail.com>
2021-01-18 17:26:40 -0600

    Worked on compose and post routes. Some styling.

--- app.js
+++ app.js
@@ -6,35 +6,39 @@ const app = express()
 const port = 3000
 const https = require('https')
 const KEY = "8c396ad3956481a4406484fd5a178317"
+app.use(express.static(__dirname + '/public'));
 
-const uri = "mongodb+srv://will:xo52eg15@cluster0.qzq8l.mongodb.net/blog?retryWrites=true&w=majority?authSource=admin";
-
+const uri = "mongodb+srv://will:xo52eg15@cluster0.qzq8l.mongodb.net/blog?retryWrites=true&w=majority";
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 const mongoose = require('mongoose');
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
 var Schema = mongoose.Schema;
-mongoose.connect(uri, {useNewUrlParser: true});
-
-
-const MyModel = mongoose.model('Test', new Schema({ name: String }));
-
 
 mongoose.model('posts', new Schema(
 {title:String,
 text:String,
-comments:Object}
+comments:Object},
+{ timestamps: true }
 	));
 
 var post = mongoose.model('posts');
-
 
 
 app.get('/', (req, res) => {
 
 //pull blog posts from DB
 //renders home page
-  
-
+	post.find({}, function(err, data) { 
+		// console.log(data);
+    //passes array through to home page
+		res.render("home", {renderList: data});
+				
+	});
   // res.render("home", {renderlist: bloglist});
   
 });
@@ -44,8 +48,14 @@ app.get('/', (req, res) => {
 
 app.get('/post/:id', (req, res) => {
     //Render full blog post.
-    
-    res.render('post', {blogPost: postToRender});
+    console.log(req.params.id)
+    post.find({ _id : req.params.id}, function(err, data) { 
+		console.log(data);
+    //passes array through to home page
+		// res.render("home", {renderList: data});
+				
+	});
+    // res.render('post', {blogPost: postToRender});
 });
 
 
@@ -61,11 +71,25 @@ app.get('/compose', (req, res) => {
 
 app.post("/compose", (req, res) => {
 	//route to post new blog post.
-
+	console.log(req);
 		// "title": req.body.title,
 		// "body": req.body.body
+	let object = {
+	title: req.body.title,
+	text: req.body.body,
+	comments: {}
+}
 
-	res.redirect('/')
+post.create(object, function(err, result) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(result);
+      res.redirect('/');
+    }
+  });
+
+	
 })
 
 
